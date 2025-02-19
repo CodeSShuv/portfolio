b1a4bfd0242296888d488bab48dd36f7bcdebc00
CodeSShuv <codeshuv@gmail.com>
2025-02-19 17:16:25 +0545

    fixed images url

--- src/Components/Project.tsx
+++ src/Components/Project.tsx
@@ -9,7 +9,7 @@ const Project = () => {
 
         <div className="card-container flex justify-evenly items-center gap-8 p-6 w-full  flex-wrap">
             <Card title='Portfolio Site' needBtn={true} />
-            <Card title='Confession Site' needBtn={true}imageUrl='./public/images/projects/confession.png'repoLink={"https://github.com/CodeSShuv/confession"} />
+            <Card title='Confession Site' needBtn={true}imageUrl='/images/projects/confession.png'repoLink={"https://github.com/CodeSShuv/confession"} />
             <Card title='Ecommerce' needBtn={true} />
             <Card title='Note App' needBtn={true} repoLink='https://github.com/CodeSShuv/noteApp' />
         </div>
