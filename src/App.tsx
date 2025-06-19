6b718bebe0e5d8e4bc54b294b35ee1fef484de98
Shuvam Gautam <codeshuv@gmail.com>
2025-06-19 21:24:42 +0545

    fixed minor bug

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faFacebook, faGithub, faInstagram, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import {  faGithub,  faLinkedin } from '@fortawesome/free-brands-svg-icons';
 import { faEnvelope } from '@fortawesome/free-regular-svg-icons';
 import Typewriter from "typewriter-effect";
 import img from "./assets/profileImage.jpg"
