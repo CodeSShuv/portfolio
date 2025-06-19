91cc4130d0c61d99a0523a451ed86ae9a22d38fa
Shuvam Gautam <codeshuv@gmail.com>
2025-06-19 23:44:45 +0545

    ''fixed minor bug"

--- src/Components/Aboutme.tsx
+++ src/Components/Aboutme.tsx
@@ -6,18 +6,21 @@ const Aboutme = () => {
             <section className='about-me flex flex-col items-start'>
                 <h2 className='text-3xl p-4 text-black vt323-regular'>Who am I? </h2>
                 <div className='p-5'>
-                    
-                        <ul className='text-black text-1xl vt323-regular
+
+                    <ul className='text-black text-1xl vt323-regular
                         charList'>
-                            <li />I'm Shuvam Gautam, a self-taught programmer and a BCA student in my 3rd semester.
+                        <li >I'm Shuvam Gautam, a self-taught programmer and a BCA student in my 3rd semester.</li>
 
-                            <li />I specialize in full-stack web development using React, Node.js, MongoDB, and TypeScript.
+                        <li>
+                            I specialize in full-stack web development using React, Node.js, MongoDB, and TypeScript.
+                        </li>
 
-                            <li />I enjoy solving problems and building intuitive, user-friendly websites.
+                        <li >I enjoy solving problems and building intuitive, user-friendly websites.
+                        </li>
+                        <li >I'm passionate about continuously learning new technologies and improving my skills.
+                        </li>
+                    </ul>
 
-                            <li />I'm passionate about continuously learning new technologies and improving my skills.
-                        </ul>
-                    
                 </div>
 
             </section>
